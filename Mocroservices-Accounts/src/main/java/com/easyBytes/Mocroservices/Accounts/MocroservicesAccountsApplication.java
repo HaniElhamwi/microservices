0c5ca511639763a26a9b659ef7e1bf57a0b7fc3b
HaniElhamwi <hanialhamwi000000@gmail.com>
2024-01-02 23:47:46 +0300

    introduction to microservices

---
+++ Mocroservices-Accounts/src/main/java/com/easyBytes/Mocroservices/Accounts/MocroservicesAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.easyBytes.Mocroservices.Accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocroservicesAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocroservicesAccountsApplication.class, args);
+	}
+
+}
